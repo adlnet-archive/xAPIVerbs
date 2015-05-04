368a0e494ddd0d71421cbbc36f22f4440af124a6
netzephir <netzephir@gmail.com>
2015-05-04 16:58:24 +0200

    Add french traduction

--- verbs.js
+++ verbs.js
@@ -4,150 +4,175 @@
          "id" : "http://adlnet.gov/expapi/verbs/answered",
          "display" : {"de-DE" : "beantwortete",
                       "en-US" : "answered",
+                      "fr-FR" : "répondu",
                       "es-ES" : "contestó"}
       },
       "asked" : {
          "id" : "http://adlnet.gov/expapi/verbs/asked",
          "display" : {"de-DE" : "fragte",
                       "en-US" : "asked",
+                      "fr-FR" : "demandé",
                       "es-ES" : "preguntó"}
       },
       "attempted" : {
          "id" : "http://adlnet.gov/expapi/verbs/attempted",
          "display" : {"de-DE" : "versuchte",
                       "en-US" : "attempted",
+                      "fr-FR" : "essayé",
                       "es-ES" : "intentó"}
       },
       "attended" : {
          "id" : "http://adlnet.gov/expapi/verbs/attended",
          "display" : {"de-DE" : "nahm teil an",
                       "en-US" : "attended",
+                      "fr-FR" : "vue",
                       "es-ES" : "asistió"}
       },
       "commented" : {
          "id" : "http://adlnet.gov/expapi/verbs/commented",
          "display" : {"de-DE" : "kommentierte",
                       "en-US" : "commented",
+                      "fr-FR" : "commenté",
                       "es-ES" : "comentó"}
       },
       "completed" : {
          "id" : "http://adlnet.gov/expapi/verbs/completed",
          "display" : {"de-DE" : "beendete",
                       "en-US" : "completed",
+                      "fr-FR" : "terminé",
                       "es-ES" : "completó"}
       },
       "exited" : {
          "id" : "http://adlnet.gov/expapi/verbs/exited",
          "display" : {"de-DE" : "verließ",
                       "en-US" : "exited",
+                      "fr-FR" : "quitté",
                       "es-ES" : "salió"}
       },
       "experienced" : {
          "id" : "http://adlnet.gov/expapi/verbs/experienced",
          "display" : {"de-DE" : "erlebte",
                       "en-US" : "experienced",
+                      "fr-FR" : "testé",
                       "es-ES" : "experimentó"}
       },
       "failed" : {
          "id" : "http://adlnet.gov/expapi/verbs/failed",
          "display" : {"de-DE" : "verfehlte",
                       "en-US" : "failed",
+                      "fr-FR" : "échoué",
                       "es-ES" : "fracasó"}
       },
       "imported" : {
          "id" : "http://adlnet.gov/expapi/verbs/imported",
          "display" : {"de-DE" : "importierte",
                       "en-US" : "imported",
+                      "fr-FR" : "importé",
                       "es-ES" : "importó"}
       },
       "initialized" : {
          "id" : "http://adlnet.gov/expapi/verbs/initialized",
          "display" : {"de-DE" : "initialisierte",
                       "en-US" : "initialized",
+                      "fr-FR" : "initialisé",
                       "es-ES" : "inicializó"}
       },
       "interacted" : {
          "id" : "http://adlnet.gov/expapi/verbs/interacted",
          "display" : {"de-DE" : "interagierte",
                       "en-US" : "interacted",
+                      "fr-FR" : "interagis",
                       "es-ES" : "interactuó"}
       },
       "launched" : {
          "id" : "http://adlnet.gov/expapi/verbs/launched",
          "display" : {"de-DE" : "startete",
                       "en-US" : "launched",
+                      "fr-FR" : "lancé",
                       "es-ES" : "lanzó"}
       },
       "mastered" : {
          "id" : "http://adlnet.gov/expapi/verbs/mastered",
          "display" : {"de-DE" : "meisterte",
                       "en-US" : "mastered",
+                      "fr-FR" : "answered",
                       "es-ES" : "dominó"}
       },
       "passed" : {
          "id" : "http://adlnet.gov/expapi/verbs/passed",
          "display" : {"de-DE" : "bestand",
                       "en-US" : "passed",
+                      "fr-FR" : "réussi",
                       "es-ES" : "aprobó"}
       },
       "preferred" : {
          "id" : "http://adlnet.gov/expapi/verbs/preferred",
          "display" : {"de-DE" : "bevorzugte",
                       "en-US" : "preferred",
+                      "fr-FR" : "préféré",
                       "es-ES" : "prefirió"}
       },
       "progressed" : {
          "id" : "http://adlnet.gov/expapi/verbs/progressed",
          "display" : {"de-DE" : "machte Fortschritt mit",
                       "en-US" : "progressed",
+                      "fr-FR" : "progressé",
                       "es-ES" : "progresó"}
       },
       "registered" : {
          "id" : "http://adlnet.gov/expapi/verbs/registered",
          "display" : {"de-DE" : "registrierte",
                       "en-US" : "registered",
+                      "fr-FR" : "enregistré",
                       "es-ES" : "registró"}
       },
       "responded" : {
          "id" : "http://adlnet.gov/expapi/verbs/responded",
          "display" : {"de-DE" : "reagierte",
                       "en-US" : "responded",
+                      "fr-FR" : "répondu",
                       "es-ES" : "respondió"}
       },
       "resumed" : {
          "id" : "http://adlnet.gov/expapi/verbs/resumed",
          "display" : {"de-DE" : "setzte fort",
                       "en-US" : "resumed",
+                      "fr-FR" : "repris",
                       "es-ES" : "continuó"}
       },
       "scored" : {
          "id" : "http://adlnet.gov/expapi/verbs/scored",
          "display" : {"de-DE" : "erreichte",
                       "en-US" : "scored",
+                      "fr-FR" : "noté",
                       "es-ES" : "anotó"}
       },
       "shared" : {
          "id" : "http://adlnet.gov/expapi/verbs/shared",
          "display" : {"de-DE" : "teilte",
                       "en-US" : "shared",
+                      "fr-FR" : "partagé",
                       "es-ES" : "compartió"}
       },
       "suspended" : {
          "id" : "http://adlnet.gov/expapi/verbs/suspended",
          "display" : {"de-DE" : "pausierte",
                       "en-US" : "suspended",
+                      "fr-FR" : "suspendu",
                       "es-ES" : "aplazó"}
       },
       "terminated" : {
          "id" : "http://adlnet.gov/expapi/verbs/terminated",
          "display" : {"de-DE" : "beendete",
                       "en-US" : "terminated",
+                      "fr-FR" : "terminé",
                       "es-ES" : "terminó"}
       },
       "voided" : {
          "id" : "http://adlnet.gov/expapi/verbs/voided",
          "display" : {"de-DE" : "entwertete",
                       "en-US" : "voided",
+                      "fr-FR" : "annulé",
                       "es-ES" : "anuló"}
       }
    };
